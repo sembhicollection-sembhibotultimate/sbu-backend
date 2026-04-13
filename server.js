1d5e8733a1a473f3cd24cf987365c90041555bd5
sembhicollection-sembhibotultimate <sembhicollection@gmail.com>
2026-04-13 07:59:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const webhookRoutes = require('./routes/webhook');
 const authRoutes = require('./routes/auth');
 const checkoutRoutes = require('./routes/checkout');
 const licenseRoutes = require('./routes/license');
-
+const adminToolsRoutes = require('./routes/adminTools');
 const app = express();
 
 connectDB();
@@ -17,6 +17,9 @@ connectDB();
 // Stripe webhook route sab ton pehla hona chahida
 app.use('/api/webhook', webhookRoutes);
 
+
+// after other routes
+app.use('/api/admin-tools', adminToolsRoutes);
 // Normal middleware
 app.use(express.json());
 app.use(cors({
