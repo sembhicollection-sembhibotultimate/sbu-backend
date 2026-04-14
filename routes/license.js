b5f34769d0cb97ca7a3f508e79fc8f9d093582fa
sembhicollection-sembhibotultimate <sembhicollection@gmail.com>
2026-04-14 04:50:25 -0400

    Update license.js

--- routes/license.js
+++ routes/license.js
@@ -55,7 +55,7 @@ router.post('/validate', async (req, res) => {
       });
     }
 
-    if (license.validUntil && new Date(license.validUntil) < new Date()) {
+    if (!license.ownerLifetime && license.validUntil && new Date(license.validUntil) < new Date()) {
       license.status = 'expired';
       await license.save();
 
@@ -73,14 +73,13 @@ router.post('/validate', async (req, res) => {
       });
     }
 
-    // First machine bind
-    if (!license.machineId && machineId) {
+    if (license.allowUnlimitedDevices || license.licenseType === 'owner' || license.ownerLifetime) {
+      license.activatedDevices = Math.max(Number(license.activatedDevices || 0), 1);
+    } else if (!license.machineId && machineId) {
       license.machineId = machineId;
       license.machineName = machineName || '';
       license.activatedDevices = 1;
-    }
-    // If already linked to another machine
-    else if (license.machineId && machineId && license.machineId !== machineId) {
+    } else if (license.machineId && machineId && license.machineId !== machineId) {
       await AuditLog.create({
         eventType: 'bot_activation_failed',
         email: license.email,
@@ -120,7 +119,10 @@ router.post('/validate', async (req, res) => {
         machineId: license.machineId,
         machineName: license.machineName,
         activatedDevices: license.activatedDevices,
-        maxDevices: license.maxDevices
+        maxDevices: license.maxDevices,
+        licenseType: license.licenseType,
+        ownerLifetime: license.ownerLifetime,
+        allowUnlimitedDevices: license.allowUnlimitedDevices
       }
     });
   } catch (error) {
